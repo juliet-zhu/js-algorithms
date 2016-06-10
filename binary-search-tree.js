aeca2acf8f2d16b4ae41577207b3592a0cbcab91
Juliet <Juliet@JulietdeMacBook-Pro.local>
2016-06-10 21:03:59 +0800

    Add bst methods.

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -4,6 +4,7 @@ class Node{
 		this.data= data;
 		this.left = left;
 		this.right = right;
+		this.count = 1;
 	}
 	show(){
 		console.log(this.data);
@@ -13,6 +14,7 @@ class Node{
 class BST{
 	constructor(){
 		this.root = null;
+		this.numbers = 0;
 	}
 	insert(data){
 		var node = new Node(data,null,null);
@@ -78,7 +80,95 @@ class BST{
 			}
 		}
 	}
-	remove(){}
+	remove(data){
+		this.root = this.removeNode(this.root,data);
+	}
+	removeNode(node,data){
+		if (node === null){
+			return null;
+		}
+		if(data === node.data){
+			if(node.left === null && node.right === null){
+				return null;
+			}
+			if(node.left === null){
+				return node.right;
+			}
+			if(node.right === null){
+				return node.left;
+			}
+			var tempNode = this.getSmallest(node.right);
+			node.data = tempNode.data;
+			node.right = this.removeNode(node.right,tempNode.data);
+		}else if (data < node.data){
+			node.left = this.removeNode(node.left,data);
+		}else{
+			node.right = this.removeNode(node.right,data);	
+		}
+		return node;
+	}
+	getSmallest(node){
+		if(node === null){
+			return null;
+		}
+		var current = node;
+		while(current.left !== null){
+			current = current.left;
+		}
+		return current;
+	}
+	getMin(){
+		if(this.root === null){
+			return null;
+		}
+		var current = this.root;
+
+		while(current.left !== null){
+			current = current.left;
+		}
+		return current.data;
+	}
+	getMax(){
+		if(this.root === null){
+			return null;
+		}
+		var current = this.root;
+		while(current.right !== null){
+			current = current.right;
+		}
+		return current.data;
+	}
+	find(data){
+		var current = this.root;
+		while(current !== null){
+			if(data === current.data){
+				return current;
+			}else if(data < current.data){
+				current = current.left;
+			}else{
+				current = current.right;
+			}
+		}
+		return null;
+	}
+	update(data){
+		var found = this.find(data);
+		if(found !== null){
+			found.count ++;
+		}
+		return found;
+	}
+	getNodesLength(node){
+		if(node !== null){
+			this.getNodesLength(node.left);
+			this.numbers++;
+			this.getNodesLength(node.right);
+		}
+	}
+	nodeNumbers(){
+		this.getNodesLength(this.root);
+		return this.numbers;
+	}
 }
 
 var bst = new BST();
@@ -89,15 +179,36 @@ bst.insert(37);
 bst.insert(3);
 bst.insert(99);
 bst.insert(22);
+
 console.log('Inorder traversal');
 bst.inOrder(bst.root);
+
 console.log('Preorder traversal');
 bst.preOrder(bst.root);
+
 console.log('Levelorder traversal');
 bst.levelOrder(bst.root);
 
+console.log('Get smallest data');
+var min = bst.getMin();
+console.log(min);
 
+console.log('Get biggest data');
+var max = bst.getMax();
+console.log(max);
 
+console.log('Find data node');
+var foundNode = bst.find(99);
+console.log(foundNode);
+
+console.log('Update count');
+var update = bst.update(23);
+console.log(update.count);
+bst.remove(45);
+bst.levelOrder(bst.root);
+
+console.log('Get numbers of node');
+console.log(bst.nodeNumbers());
 
 
 
