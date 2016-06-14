10c59fdc15a541bc5d90ed76fd51973b17950b47
juliet-zhu <juliet.zhu.work@gmail.com>
2016-06-14 22:17:48 +0800

    Add selection sort and insertion sort.

--- sorting.js
+++ sorting.js
@@ -50,10 +50,36 @@ class CArray{
 			}
 		}
 	}
+	selectionSort(){
+		for(let i = 0; i < this.dataStore.length-1; i++){
+			for(let k = i+1; k <this.dataStore.length; k++){
+				if(this.dataStore[k] < this.dataStore[i]){
+					this.swap(this.dataStore,i,k);
+				}
+			}
+		}
+	}
+	insertionSort(){
+		for(let i = 1; i < this.dataStore.length; i++){
+			let value = this.dataStore[i];
+			let index = i;
+			while(index > 0 && value < this.dataStore[index-1]){
+				this.dataStore[i] = this.dataStore[i-1]
+				index --; 
+			}
+			this.dataStore[index] = value;
+		}
+	}
+
 }
 var arr = new CArray(10);
 arr.setData();
 console.log(arr.dataStore);
+// console.log(arr.toString());
+// arr.bubbleSort();
+// console.log(arr.toString());
+// arr.selectionSort();
+// console.log(arr.toString());
+arr.insertionSort();
 console.log(arr.toString());
-arr.bubbleSort();
-console.log(arr.toString());
+
